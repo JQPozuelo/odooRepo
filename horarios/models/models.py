364499f68b0da975dc249650acd2e94996abfe8e
Yorjo15 <91139391+Yorjo15@users.noreply.github.com>
2022-02-24 21:09:03 +0100

    hora

--- horarios/models/models.py
+++ horarios/models/models.py
@@ -31,18 +31,36 @@ class horario(models.Model):
 
     #Atributos
     nombreHorario = fields.Char(string='Nombre horario', required=True)
-    lunesEntrada = fields.Date(string='Hora entrada lunes')
-    lunesSalida = fields.Date(string='Hora salida lunes')
 
-    martesEntrada = fields.Date(string='Hora entrada martes')
-    martesSalida = fields.Date(string='Hora salida martes')
+    lunesEntrada = fields.Selection(string='Hora entrada lunes', selection='_get_valid_hours', default='8.5')
+    lunesSalida = fields.Selection(string='Hora salida lunes', selection='_get_valid_hours', default='8.5')
 
-    miercolesEntrada = fields.Date(string='Hora entrada miercoles')
-    miercolesSalida = fields.Date(string='Hora salida miercoles')
+    martesEntrada = fields.Selection(string='Hora entrada martes', selection='_get_valid_hours', default='8.5')
+    martesSalida = fields.Selection(string='Hora salida martes', selection='_get_valid_hours', default='8.5')
 
-    juevesEntrada = fields.Date(string='Hora entrada jueves')
-    juevesSalida = fields.Date(string='Hora salida jueves')
+    miercolesEntrada = fields.Selection(string='Hora entrada miercoles', selection='_get_valid_hours', default='8.5')
+    miercolesSalida = fields.Selection(string='Hora salida miercoles', selection='_get_valid_hours', default='8.5')
 
-    viernesEntrada = fields.Date(string='Hora entrada viernes')
-    viernesSalida = fields.Date(string='Hora salida viernes')
+    juevesEntrada = fields.Selection(string='Hora entrada jueves', selection='_get_valid_hours', default='8.5')
+    juevesSalida = fields.Selection(string='Hora salida jueves', selection='_get_valid_hours', default='8.5')
+
+    viernesEntrada = fields.Selection(string='Hora entrada viernes', selection='_get_valid_hours', default='8.5')
+    viernesSalida = fields.Selection(string='Hora salida viernes', selection='_get_valid_hours', default='8.5')
+
+    @api.model
+    def _get_valid_hours(self):
+        selection = [
+            ('8', '8:00'),   ('8.5', '8:30'),
+            ('9', '9:00'),   ('9.5', '9:30'),
+            ('10', '10:00'), ('10.5', '10:30'),
+            ('11', '11:00'), ('11.5', '11:30'),
+            ('12', '12:00'), ('12.5', '12:30'),
+            ('13', '13:00'), ('13.5', '13:30'),
+            ('14', '14:00'), ('14.5', '14:30'),
+            ('15', '15:00'), ('15.5', '15:30'),
+            ('16', '16:00'), ('16.5', '16:30'),
+            ('17', '17:00'), ('17.5', '17:30'),
+            ('18', '18:00')
+        ]
+        return selection
     
