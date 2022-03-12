63896bf29d6e76e35fe012e0d8d8693daef3924a
Yorjo15 <91139391+Yorjo15@users.noreply.github.com>
2022-03-12 20:51:21 +0100

    tyt

--- mantenimientos/models/models.py
+++ mantenimientos/models/models.py
@@ -37,7 +37,7 @@ class descripcion(models.Model):
     def _getPr(self):
         hoy = date.today()
         for descripcion in self:
-            descripcion.prueba = relativedelta(hoy, descripcion.fecha).years
+            descripcion.prueba = relativedelta(hoy, descripcion.Fecha).years
 
 
     #Relacion
