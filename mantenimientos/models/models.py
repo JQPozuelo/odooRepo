0cf49dbdabcc707f2059824d1c7008baf843c1d0
Yorjo15 <91139391+Yorjo15@users.noreply.github.com>
2022-03-14 16:26:50 +0100

    fecha2

--- mantenimientos/models/models.py
+++ mantenimientos/models/models.py
@@ -30,14 +30,9 @@ class descripcion(models.Model):
     Precio = fields.Integer(string='Precio del mantenimiento', required=True)
     Apuntes = fields.Text(string='Notas de la reparacion', required=True, help='Apunta bien lo que haga falta')
     Fecha = fields.Date(string='Fecha de recepcion', required=True, default= fields.date.today())
-    Prueba = fields.Integer('Prueba', compute='_getPr')
 
     #Prueba validacion
-    @api.depends('Fecha')
-    def _getPr(self):
-        hoy = date.today()
-        for descripcion in self:
-            descripcion.Prueba = relativedelta(hoy, descripcion.Fecha).years
+    
 
 
     #Relacion
@@ -57,13 +52,20 @@ class coche(models.Model):
 
     #Atributos de la clase
     Modelo = fields.Char(string='Modelo del vehiculo', required=True)
-    Anio = fields.Integer(string='Año de fabricacion', required=True)
+    Anio = fields.Integer('Años del vehiculo', compute='_getPr')
     Motor = fields.Char(string='Tipo de motor', required=True)
     Combustible = fields.Selection(string='Combustible del vehiculo', selection=[('a', 'Gasolina'), ('b', 'Diesel')])
     NombrePropietario = fields.Char(string='Nombre del propietario', required=True)
     TelefonoPropietario = fields.Char(string='Telefono del propietario', required=True)
+    FechaFa = fields.Date(string='Fecha de fabricacion', required=True, default= fields.date.today())
     
     #Validaciones y metodos
+    @api.depends('FechaFa')
+    def _getPr(self):
+        hoy = date.today()
+        for descripcion in self:
+            descripcion.Prueba = relativedelta(hoy, descripcion.Fecha).years
+
     def name_get(self):
         listaCoches =  []
         for coches in self:
