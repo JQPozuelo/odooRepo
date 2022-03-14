98a982e3db93447a4199935e212efaa9864b1166
Yorjo15 <91139391+Yorjo15@users.noreply.github.com>
2022-03-14 16:45:57 +0100

    ifnorme

--- mantenimientos/__manifest__.py
+++ mantenimientos/__manifest__.py
@@ -28,6 +28,7 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'report/mante_infDescrip.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
